895e6a17c8903ebf4dedbeb614fc86c732b73b3c
LucianoVegaSs5 <98593132+LucianoVegaSs5@users.noreply.github.com>
2022-02-18 12:46:11 -0300

    Tps del 1 al 5

---
+++ jsIteracionesWhile-05.js
@@ -0,0 +1,17 @@
+/*
+Al presionar el botón pedir un sexo
+'f' para femenino, 'm' para masculino.*/
+function mostrar()
+{
+	var sexoIngresado;
+	sexoIngresado = prompt("ingrese f ó m .");
+	sexoIngresado = sexoIngresado.toLowerCase();
+
+	while(sexoIngresado !="f" && sexoIngresado !="m") // o while(!(sexoIngresado =="f" && sexoIngresado =="m")); 
+	{
+		sexoIngresado = prompt("reingrese f ó m .");
+		sexoIngresado = sexoIngresado.toLowerCase();
+	}
+
+	document.getElementById("txtIdSexo").value = sexoIngresado;
+}//FIN DE LA FUNCIÓN
